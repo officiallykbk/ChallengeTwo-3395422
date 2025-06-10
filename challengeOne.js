4e09f3816e78f492b3b0ceaa2f272b0c0a5dc744
Kwabena Boateng Konadu <officiallykbk@gmail.com>
2025-06-10 23:59:34 +0000

    challengeOne

---
+++ challengeOne.js
@@ -0,0 +1,48 @@
+// Trie() Initializes the trie object. 
+// • void insert(String word) Inserts the string word into the trie. 
+// • boolean search(String word) Returns true if the string word is in the trie (i.e., was 
+// inserted before), and false otherwise. 
+// • boolean startsWith(String prefix) Returns true if there is a previously inserted 
+// string word that has the prefix prefix, and false otherwise.
+
+class TrieNode {
+  constructor() {
+    this.children = {};
+    this.isEndOfWord = false;
+  }
+}
+
+class Trie {
+  constructor() {
+    this.root = new TrieNode();
+  }
+
+  insert(word) {
+    let node = this.root;
+    for (let char of word) {
+      if (!node.children[char]) {
+        node.children[char] = new TrieNode();
+      }
+      node = node.children[char];
+    }
+    node.isEndOfWord = true;
+  }
+
+  search(word) {
+    let node = this.root;
+    for (let char of word) {
+      if (!node.children[char]) return false;
+      node = node.children[char];
+    }
+    return node.isEndOfWord;
+  }
+
+  startsWith(prefix) {
+    let node = this.root;
+    for (let char of prefix) {
+      if (!node.children[char]) return false;
+      node = node.children[char];
+    }
+    return true;
+  }
+}
